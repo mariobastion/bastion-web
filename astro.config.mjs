7bfdc973798986932ad5183938ffc05e307c7237
root <root@MARIO-LAPTOP.localdomain>
2026-04-10 16:06:48 +0200

    docs: configurar sitemap y pulir footer. Añadir linkedin 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()]
+  site: 'https://bastiondefensa.com', // CAMBIA ESTO por tu dominio real
+  integrations: [sitemap()],
 });
